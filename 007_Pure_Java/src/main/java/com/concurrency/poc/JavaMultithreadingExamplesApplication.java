f9f898746de866fb3667b361e1d286e0b6e63802
Caglar Gonul <caglar.gonul.001@gmail.com>
2021-01-16 18:00:42 +0300

    Learning concurrency firs commit.

---
+++ 007_Pure_Java/src/main/java/com/concurrency/poc/JavaMultithreadingExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.concurrency.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMultithreadingExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMultithreadingExamplesApplication.class, args);
+	}
+
+}
---
+++ 007_Pure_Java/src/test/java/com/concurrency/poc/JavaMultithreadingExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurrency.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMultithreadingExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
